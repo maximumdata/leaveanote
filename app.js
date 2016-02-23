206d99115959dd53e9a96f7dfc70b23733f01a0a
Mike Dettmer <mike@mikedettmer.com>
2016-02-23 10:11:01 -0500

    added routing and elements for upvoting and downvoting each note

--- app.js
+++ app.js
@@ -3,25 +3,30 @@
 var leaveANote = angular.module('leaveANote', ['ngResource','angularUtils.directives.dirPagination']);
 
 
-leaveANote.factory('getAllNotes', function($resource) {
-  return $resource('http://mikedettmer.com/api/projects/notes/');
+leaveANote.factory('Notes', function($resource) {
+  return $resource('http://mikedettmer.com/api/projects/notes/:id/:meta', {id: '@id', meta: '@meta'});
 });
 
-leaveANote.controller('MainCtrl', function(getAllNotes, $scope) {
-  $scope.data = getAllNotes.query();
+leaveANote.controller('MainCtrl', function(Notes, $scope) {
+  $scope.data = Notes.query();
 
   $scope.saveNote = function() {
-    var newPost = new getAllNotes();
+    var newPost = new Notes();
     newPost.name = $scope.form.name;
     newPost.body = $scope.form.body;
     newPost.date = Date.now();
     if(newPost.name && newPost.body) {
       newPost.$save(function(){
-        $scope.data = getAllNotes.query();
+        $scope.data = Notes.query();
       });
     } else {
       $scope.error = 'Make sure to fill out both fields!';
     }
   }
 
+  $scope.vote = function(id, direction) {
+    var note = Notes.save({id: id, meta: direction}, function() {
+      $scope.data = Notes.query();
+    });
+  }
 });
